79c0549990cf3f59e42658bc103b23af347a95eb
Chris Engleby <cengleby86@gmail.com>
2025-05-01 16:53:13 -0400

    add domain as output

--- modules/azure/aro/main.tf
+++ modules/azure/aro/main.tf
@@ -1,19 +1,30 @@
 locals {
-  domain = var.domain != null && var.domain != "" ? var.domain : random_string.domain.result
+    domain = var.domain != null && var.domain != "" ? var.domain : random_string.domain.result
 }
 
 resource "random_string" "domain" {
-  length  = 8
-  special = false
-  upper   = false
-  numeric = false
+    length  = 8
+    special = false
+    upper   = false
+    numeric = false
+}
+
+data "azurerm_client_config" "cluster" {}
+
+data "azuread_client_config" "cluster" {}
+
+resource "azuread_application" "cluster" {
+    display_name = "aro-${local.domain}"
+}
+
+resource "azuread_service_principal" "cluster" {
+    client_id = azuread_application.cluster.client_id
 }
 
 data "azuread_service_principal" "redhatopenshift" {
-  // This is the Azure Red Hat OpenShift RP service principal id, do NOT delete it
+  // This is the Azure Red Hat OpenShift RP service principal id
   client_id = "f1dd0a37-89c6-4e07-bcd1-ffd3d43d8875"
 }
-
 
 output "api_url" {
   value = "stuff"
