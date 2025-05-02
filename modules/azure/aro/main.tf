c2bfaf9befc244ff17665ac4419fe8b573ad27f0
Chris Engleby <cengleby86@gmail.com>
2025-05-01 22:09:07 -0400

    building specifically for rhdh

--- modules/azure/aro/main.tf
+++ modules/azure/aro/main.tf
@@ -21,9 +21,9 @@ data "azuread_service_principal" "cluster" {
     client_id = data.azuread_application.cluster.client_id
 }
 
-resource "azuread_service_principal_password" "example" {
-    service_principal_id = data.azuread_service_principal.cluster.object_id
-}
+#resource "azuread_service_principal_password" "cluster" {
+#    service_principal_id = data.azuread_service_principal.cluster.object_id
+#}
 
 data "azuread_service_principal" "redhatopenshift" {
   // This is the Azure Red Hat OpenShift RP service principal id
@@ -31,13 +31,17 @@ data "azuread_service_principal" "redhatopenshift" {
 }
 
 output "api_url" {
-  value = "stuff"
+    value = "stuff"
 }
 
 output "console_url" {
-  value = "stuff"
+    value = "stuff"
 }
 
 output "domain" {
-  value = local.domain
+    value = local.domain
+}
+
+output "service_principal_id" {
+    value = data.azuread_service_principal.cluster.object_id
 }
