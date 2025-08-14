348b97e3a9251d1cd5b586e22842b289c0a86a4c
Chris Engleby <cengleby86@gmail.com>
2025-08-13 23:28:44 -0400

    correct passed vars

--- modules/azure/ocp/variables.tf
+++ modules/azure/ocp/variables.tf
@@ -24,6 +24,53 @@ variable "tags" {
     }
 }
 
+variable "aro_version" {
+  type        = string
+  description = <<EOF
+  ARO version
+  Default "4.16.30"
+  EOF
+  default     = "4.16.30"
+}
+
+variable "pull_secret_path" {
+  type        = string
+  default     = null
+  description = <<EOF
+  Pull Secret for the ARO cluster
+  Default null
+  EOF
+}
+
+variable "pull_secret" {
+  type        = string
+  default     = null 
+  description = <<EOF
+  Pull Secret provided from config file
+  EOF
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID (needed with the new Auth method)"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Subscription ID (needed with the new Auth method)"
+}
+
+variable "worker_node_count" {
+  type        = number
+  default     = 3
+  description = "Number of worker nodes."
+
+  validation {
+    condition     = var.worker_node_count >= 3
+    error_message = "Invalid 'worker_node_count'. Minimum of 3."
+  }
+}
+
 # variable "vnet_cidr" {
 #   description = "CIDR for the VNet."
 #   type        = string
