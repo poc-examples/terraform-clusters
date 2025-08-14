80a388d17578dfc7f791c081698004e734c6a97f
Chris Engleby <cengleby86@gmail.com>
2025-08-14 00:28:10 -0400

    add ingress/api lbs

--- modules/azure/ocp/main.tf
+++ modules/azure/ocp/main.tf
@@ -221,37 +221,38 @@ resource "azurerm_public_ip" "public_ip_ingress" {
     tags                = var.tags
 }
 
+# --------------------------
+# Load Balancer
+# --------------------------
+# Public API LB (6443)
+resource "azurerm_lb" "lb_api_public" {
+    name                = "${var.cluster_name}-lb-api-public"
+    location            = data.azurerm_resource_group.location
+    resource_group_name = data.azurerm_resource_group.name
+    sku                 = "Standard"
+    tags                = var.tags
 
+    frontend_ip_configuration {
+        name                 = "fe"
+        public_ip_address_id = azurerm_public_ip.public_ip_api.id
+    }
+}
 
+resource "azurerm_lb_backend_address_pool" "lbp_api_public" {
+  name            = "be"
+  loadbalancer_id = azurerm_lb.lb_api_public.id
+}
 
-
-
-
-
-
-
-
-# # --------------------------
-# # Load Balancers
-# # --------------------------
-# # Public API LB (6443)
-# resource "azurerm_lb" "lb_api_public" {
-#   name                = "${var.cluster_name}-lb-api-public"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   sku                 = "Standard"
-#   tags                = var.tags
-
-#   frontend_ip_configuration {
-#     name                 = "fe"
-#     public_ip_address_id = azurerm_public_ip.pip_api.id
-#   }
-# }
-
-# resource "azurerm_lb_backend_address_pool" "lbp_api_public" {
-#   name            = "be"
-#   loadbalancer_id = azurerm_lb.lb_api_public.id
-# }
+resource "azurerm_lb_rule" "rule_api_6443" {
+  name                           = "api-6443"
+  loadbalancer_id                = azurerm_lb.lb_api_public.id
+  protocol                       = "Tcp"
+  frontend_port                  = 6443
+  backend_port                   = 6443
+  frontend_ip_configuration_name = "fe"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_api_public.id]
+#   probe_id                       = azurerm_lb_probe.probe_api_6443.id
+}
 
 # resource "azurerm_lb_probe" "probe_api_6443" {
 #   name                = "tcp-6443"
@@ -262,35 +263,50 @@ resource "azurerm_public_ip" "public_ip_ingress" {
 #   number_of_probes    = 2
 # }
 
-# resource "azurerm_lb_rule" "rule_api_6443" {
-#   name                           = "api-6443"
-#   loadbalancer_id                = azurerm_lb.lb_api_public.id
-#   protocol                       = "Tcp"
-#   frontend_port                  = 6443
-#   backend_port                   = 6443
-#   frontend_ip_configuration_name = "fe"
-#   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_api_public.id]
-#   probe_id                       = azurerm_lb_probe.probe_api_6443.id
-# }
 
-# # Public Ingress LB (80/443)
-# resource "azurerm_lb" "lb_ingress_public" {
-#   name                = "${var.cluster_name}-lb-ingress-public"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   sku                 = "Standard"
-#   tags                = var.tags
+# --------------------------
+# Load Balancer
+# --------------------------
+# Public Ingress LB (80/443)
+resource "azurerm_lb" "lb_ingress_public" {
+    name                = "${var.cluster_name}-lb-ingress-public"
+    location            = data.azurerm_resource_group.location
+    resource_group_name = data.azurerm_resource_group.name
+    sku                 = "Standard"
+    tags                = var.tags
 
-#   frontend_ip_configuration {
-#     name                 = "fe"
-#     public_ip_address_id = azurerm_public_ip.pip_ingress.id
-#   }
-# }
+    frontend_ip_configuration {
+        name                 = "fe"
+        public_ip_address_id = azurerm_public_ip.public_ip_ingress.id
+    }
+}
 
-# resource "azurerm_lb_backend_address_pool" "lbp_ingress_public" {
-#   name            = "be"
-#   loadbalancer_id = azurerm_lb.lb_ingress_public.id
-# }
+resource "azurerm_lb_backend_address_pool" "lbp_ingress_public" {
+  name            = "be"
+  loadbalancer_id = azurerm_lb.lb_ingress_public.id
+}
+
+resource "azurerm_lb_rule" "rule_ingress_80" {
+  name                           = "ingress-80"
+  loadbalancer_id                = azurerm_lb.lb_ingress_public.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "fe"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_ingress_public.id]
+#   probe_id                       = azurerm_lb_probe.probe_http_80.id
+}
+
+resource "azurerm_lb_rule" "rule_ingress_443" {
+  name                           = "ingress-443"
+  loadbalancer_id                = azurerm_lb.lb_ingress_public.id
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "fe"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_ingress_public.id]
+#   probe_id                       = azurerm_lb_probe.probe_https_443.id
+}
 
 # resource "azurerm_lb_probe" "probe_http_80" {
 #   name                = "tcp-80"
@@ -308,28 +324,6 @@ resource "azurerm_public_ip" "public_ip_ingress" {
 #   port                = 443
 #   interval_in_seconds = 5
 #   number_of_probes    = 2
-# }
-
-# resource "azurerm_lb_rule" "rule_ingress_80" {
-#   name                           = "ingress-80"
-#   loadbalancer_id                = azurerm_lb.lb_ingress_public.id
-#   protocol                       = "Tcp"
-#   frontend_port                  = 80
-#   backend_port                   = 80
-#   frontend_ip_configuration_name = "fe"
-#   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_ingress_public.id]
-#   probe_id                       = azurerm_lb_probe.probe_http_80.id
-# }
-
-# resource "azurerm_lb_rule" "rule_ingress_443" {
-#   name                           = "ingress-443"
-#   loadbalancer_id                = azurerm_lb.lb_ingress_public.id
-#   protocol                       = "Tcp"
-#   frontend_port                  = 443
-#   backend_port                   = 443
-#   frontend_ip_configuration_name = "fe"
-#   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbp_ingress_public.id]
-#   probe_id                       = azurerm_lb_probe.probe_https_443.id
 # }
 
 # # Internal API/MCS LB (6443 + 22623)
