b644893a2ad6c01d8df4782b7e14568e70e1a327
Chris Engleby <cengleby86@gmail.com>
2025-08-15 09:55:38 -0400

    enable internal probes

--- modules/azure/ocp/main.tf
+++ modules/azure/ocp/main.tf
@@ -6,6 +6,36 @@ data "azurerm_resource_group" "cluster" {
 }
 
 # --------------------------
+# Ignition Files
+# --------------------------
+resource "azurerm_storage_blob" "ign_bootstrap" {
+  name                   = "bootstrap.ign"
+  storage_account_name   = azurerm_storage_account.ign.name
+  storage_container_name = azurerm_storage_container.ign.name
+  type                   = "Block"
+  content_type           = "application/json"
+  source                 = "/usr/src/app/terraform/bootstrap.ign"  # path to your file
+}
+
+resource "azurerm_storage_blob" "ign_master" {
+  name                   = "master.ign"
+  storage_account_name   = azurerm_storage_account.ign.name
+  storage_container_name = azurerm_storage_container.ign.name
+  type                   = "Block"
+  content_type           = "application/json"
+  source                 = "/usr/src/app/terraform/master.ign"
+}
+
+resource "azurerm_storage_blob" "ign_worker" {
+  name                   = "worker.ign"
+  storage_account_name   = azurerm_storage_account.ign.name
+  storage_container_name = azurerm_storage_container.ign.name
+  type                   = "Block"
+  content_type           = "application/json"
+  source                 = "/usr/src/app/terraform/worker.ign"
+}
+
+# --------------------------
 # Networking: VNet + Subnets
 # --------------------------
 resource "azurerm_virtual_network" "network" {
@@ -644,41 +674,41 @@ resource "azurerm_network_interface" "bootstrap" {
     }
 }
 
-# resource "azurerm_linux_virtual_machine" "bootstrap" {
-#     name                = "${var.cluster_name}-vm-bootstrap"
-#     location            = data.azurerm_resource_group.cluster.location
-#     resource_group_name = data.azurerm_resource_group.cluster.name
-#     size                = "Standard_D8s_v3"
-#     admin_username      = "core"
-#     network_interface_ids = [azurerm_network_interface.bootstrap.id]
+resource "azurerm_linux_virtual_machine" "bootstrap" {
+    name                = "${var.cluster_name}-vm-bootstrap"
+    location            = data.azurerm_resource_group.cluster.location
+    resource_group_name = data.azurerm_resource_group.cluster.name
+    size                = "Standard_D8s_v3"
+    admin_username      = "core"
+    network_interface_ids = [azurerm_network_interface.bootstrap.id]
 
-#     source_image_reference {
-#         publisher = local.rhcos_publisher
-#         offer     = local.rhcos_offer
-#         sku       = local.rhcos_sku
-#         version   = local.rhcos_version
-#     }
+    source_image_reference {
+        publisher = local.rhcos_publisher
+        offer     = local.rhcos_offer
+        sku       = local.rhcos_sku
+        version   = local.rhcos_version
+    }
 
-#     admin_ssh_key {
-#         username   = "core"
-#         public_key = local.ssh_pubkey
-#     }
+    admin_ssh_key {
+        username   = "core"
+        public_key = local.ssh_pubkey
+    }
 
-#     plan {
-#         name      = "rh-ocp-worker"
-#         product   = "rh-ocp-worker"
-#         publisher = "redhat"
-#     }
+    plan {
+        name      = "rh-ocp-worker"
+        product   = "rh-ocp-worker"
+        publisher = "redhat"
+    }
 
-#     custom_data = local.bootstrap_custom_data
+    custom_data = local.bootstrap_custom_data
 
-#     os_disk {
-#         name                 = "${var.cluster_name}-os-bootstrap"
-#         caching              = "ReadWrite"
-#         storage_account_type = "Premium_LRS"
-#         disk_size_gb         = "1000"
-#     }
-# }
+    os_disk {
+        name                 = "${var.cluster_name}-os-bootstrap"
+        caching              = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+        disk_size_gb         = "1000"
+    }
+}
 
 #
 # MASTERS
