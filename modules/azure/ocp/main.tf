923abcc96425a51527fb741385d3053aa6f576ee
Chris Engleby <cengleby86@gmail.com>
2025-08-15 13:48:00 -0400

    reduce version

--- modules/azure/ocp/main.tf
+++ modules/azure/ocp/main.tf
@@ -866,9 +866,9 @@ resource "azurerm_network_interface_backend_address_pool_association" "masters_a
 }
 
 # Workers to public ingress LB
-resource "azurerm_network_interface_backend_address_pool_association" "workers_ingress_public" {
-  for_each                = { for i, nic in azurerm_network_interface.worker : i => nic.id }
-  network_interface_id    = each.value
-  ip_configuration_name   = "ipconfig1"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lbp_ingress_public.id
-}
+# resource "azurerm_network_interface_backend_address_pool_association" "workers_ingress_public" {
+#   for_each                = { for i, nic in azurerm_network_interface.worker : i => nic.id }
+#   network_interface_id    = each.value
+#   ip_configuration_name   = "ipconfig1"
+#   backend_address_pool_id = azurerm_lb_backend_address_pool.lbp_ingress_public.id
+# }
