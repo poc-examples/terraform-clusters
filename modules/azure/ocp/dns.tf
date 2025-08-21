86d51b0052f622d28d5036335f228359335a0980
Chris Engleby <cengleby86@gmail.com>
2025-08-21 04:07:16 -0400

    refactor

--- modules/azure/ocp/dns.tf
+++ modules/azure/ocp/dns.tf
@@ -26,3 +26,26 @@ resource "azurerm_private_dns_a_record" "api_int" {
     records             = [var.api_int_lb_ip]
     tags                = var.tags
 }
+
+# --------------------------
+# Public DNS
+# --------------------------
+# # api.<cluster>.<base_domain> -> public API Public IP
+# resource "azurerm_dns_a_record" "api" {
+#     name                = "api.${var.cluster_name}"
+#     zone_name           = azurerm_dns_zone.zone.name
+#     resource_group_name = data.azurerm_resource_group.cluster.name
+#     ttl                 = 60
+#     records             = [azurerm_public_ip.public_ip_api.ip_address]
+#     tags                = var.tags
+# }
+
+# # *.apps.<cluster>.<base_domain> -> public Ingress Public IP
+# resource "azurerm_dns_a_record" "apps_wildcard" {
+#     name                = "*.apps.${var.cluster_name}"
+#     zone_name           = azurerm_dns_zone.zone.name
+#     resource_group_name = data.azurerm_resource_group.cluster.name
+#     ttl                 = 60
+#     records             = [azurerm_public_ip.public_ip_ingress.ip_address]
+#     tags                = var.tags
+# }
